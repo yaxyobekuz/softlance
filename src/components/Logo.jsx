17329cad76f895f47993684bfb9f9086a9c5bb8c
yaxyobekb <pubgn9642@gmail.com>
2025-04-20 22:26:20 +0500

    enhance Logo component with transition effects for smoother color changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
 const Logo = ({ className = "" }) => {
   return (
-    <h1 className={`text-3xl font-semibold ${className}`}>
+    <h1
+      className={`text-3xl font-semibold ${className} transition-colors duration-200`}
+    >
       <span className="text-brand-default">S</span>
-      <span className="transition-colors duration-200 dark:text-white">oftlance</span>
-      <span className="inline-block scale-75 transition-colors duration-200 dark:text-white">®</span>
+      <span className="dark:text-white">oftlance</span>
+      <span className="inline-block scale-75 dark:text-white">®</span>
     </h1>
   );
 };
